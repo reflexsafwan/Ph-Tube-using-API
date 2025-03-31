443b1eaa9ee28086c204e2588c39133bd2bdcab7
reflexsafwan <reflex.safwan@gmail.com>
2025-04-01 02:26:32 +0600

    catagory button toggled done succesfully

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
+const removeActiveClass = () => {
+  const activeClass = document.getElementsByClassName("active");
+  console.log(activeClass);
+  for (let btn of activeClass) {
+    btn.classList.remove("active");
+  }
+};
+
 const loadCatagories = async () => {
   try {
     const response = await fetch(
@@ -10,17 +18,6 @@ const loadCatagories = async () => {
     console.error("Error fetching categories:", error);
   }
 };
-const showCategories = (categories) => {
-  const liContainer = document.getElementById("li-container");
-
-  categories.map((category) => {
-    const div = document.createElement("div");
-    div.innerHTML = `
-          <li onclick = "loadCatagoryVedios(${category.category_id})" class="btn btn-sm text-lg hover:bg-red-700 hover:text-white">${category.category}</li>
-    `;
-    liContainer.appendChild(div);
-  });
-};
 
 const loadAllVedios = async () => {
   try {
@@ -29,6 +26,9 @@ const loadAllVedios = async () => {
     );
     const data = await res.json();
     const vedios = data.videos;
+    removeActiveClass();
+    const btnAll = document.getElementById("btn-all");
+    btnAll.classList.add("active");
     showVedios(vedios);
   } catch (error) {
     console.log(error);
@@ -36,21 +36,50 @@ const loadAllVedios = async () => {
 };
 
 const loadCatagoryVedios = async (id) => {
+  console.log(id);
   try {
     const res = await fetch(
       ` https://openapi.programming-hero.com/api/phero-tube/category/${id}`
     );
     const data = await res.json();
-    console.log(data.category);
-    showVedios(data.category);
+
+    const vedios = data.category;
+    removeActiveClass();
+    const clickedButton = document.getElementById(`btn-${id}`);
+    clickedButton.classList.add("active");
+
+    showVedios(vedios);
   } catch (error) {
     console.log(error);
   }
 };
 
+const showCategories = (categories) => {
+  const liContainer = document.getElementById("li-container");
+
+  categories.map((category) => {
+    const div = document.createElement("div");
+    div.innerHTML = `
+          <li id="btn-${category.category_id}"  onclick = "loadCatagoryVedios(${category.category_id})" class="btn btn-sm text-lg hover:bg-red-700 hover:text-white">${category.category}</li>
+    `;
+    liContainer.appendChild(div);
+  });
+};
+
 const showVedios = (vedios) => {
   const cardContainer = document.getElementById("card-container");
   cardContainer.innerHTML = "";
+  if (vedios.length == 0) {
+    cardContainer.innerHTML = `
+     <div class="col-span-full text-center flex flex-col justify-center items-center mt-20">
+                <img class="w-40" src="./assets/Icon.png" alt="">
+                <h2 class="text-2xl font-bold mt-10">Oops!! Sorry, There is no content here</h2>
+            </div>
+    
+    `;
+
+    return;
+  }
   vedios.map((vedio) => {
     const div = document.createElement("div");
     div.innerHTML = `
